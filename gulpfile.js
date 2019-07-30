aa5e85fe2f9de5ae058fd95dadeec206d5f4ff3e
John McAvoy <mcavoyj5@students.rowan.edu>
2019-07-30 18:31:34 -0400

    Add pug workflow

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const { src, dest, series, watch } = require('gulp');
+const path = require('path')
+const pug = require('gulp-pug')
+const rename = require('gulp-rename')
+const del = require("del")
+
+const OUTPUT_DIR = path.resolve(__dirname, 'dist')
+
+function compile_pug(cb) {
+  src(['src/views/**.pug', '!src/views/mixins/**.pug'])
+    .pipe(pug())
+    .pipe(rename({
+      dirname: '',
+      extname: '.html'
+    }))
+    .pipe(dest(OUTPUT_DIR))
+
+  return cb()
+}
+
+function watch_files(cb) {
+  watch('src/**', compile_pug);
+  cb()
+}
+
+function startServer(cb) {
+  const handler = require('serve-handler')
+  const http = require('http')
+
+  const server = http.createServer((req, res) => handler(req, res, {cleanUrls: true, public: 'dist'}))
+  server.listen(5000)
+
+  return cb()
+
+}
+
+function clean(cb) {
+  del("dist", {force: true})
+  cb()
+}
+
+exports.default = compile_pug
+exports.start_server = startServer
+exports.watch = series(startServer, watch_files)
+exports.clean = clean
