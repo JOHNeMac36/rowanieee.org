d70356e77734e9c1aeb3508bd9e1d68edd45c034
John McAvoy <mcavoyj5@students.rowan.edu>
2019-08-13 10:25:33 -0400

    Add gulp specific eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,127 @@
-const { src, dest, series, parallel, watch } = require('gulp');
-const path = require('path')
-const pug = require('gulp-pug')
-const rename = require('gulp-rename')
-const del = require('del')
-const vinylPaths = require('vinyl-paths')
-const fs = require('fs')
-const typescript = require('gulp-typescript')
-const typedoc = require('gulp-typedoc')
-const eslint = require('gulp-eslint')
-const pugLinter = require('gulp-pug-linter')
-const sass = require('gulp-sass')
+const {
+  src, dest, series, parallel, watch,
+} = require('gulp');
+const path = require('path');
+const pug = require('gulp-pug');
+const rename = require('gulp-rename');
+const del = require('del');
+const typescript = require('gulp-typescript');
+const typedoc = require('gulp-typedoc');
+const eslint = require('gulp-eslint');
+const pugLinter = require('gulp-pug-linter');
+const sass = require('gulp-sass');
+const serveHandler = require('serve-handler');
+const http = require('http');
 
-const OUTPUT_DIR = path.resolve(__dirname, 'dist')
+const OUTPUT_DIR = path.resolve(__dirname, 'dist');
 
-function compile_pug() {
+// resources
+const eboards = require('./src/resources/eboards.json');
+
+function compilePug() {
   return src(['src/views/**.pug', '!src/views/mixins/**.pug'])
     .pipe(pug({
       pretty: true,
       debug: false,
       data: {
         resources: {
-          eboards: require('./src/resources/eboards.json')
-        }
-      }
+          eboards,
+        },
+      },
     }))
-    .on('error', err => { console.log(err.message); pipe.emit('end')})
+    .on('error', (err) => console.log(err.message))
     .pipe(rename({
       dirname: '',
-      extname: '.html'
+      extname: '.html',
     }))
-    .pipe(dest(OUTPUT_DIR))
+    .pipe(dest(OUTPUT_DIR));
 }
 
-function compile_ts(cb) {
+function compileTypescript(cb) {
   const pipe = src('src/scripts/**.ts')
     .pipe(typescript())
-    .on('error', err => { console.log(err.message); pipe.emit('end')})
+    .on('error', (err) => { console.log(err.message); pipe.emit('end'); })
     .pipe(rename({
       dirname: '',
-      extname: '.js'
+      extname: '.js',
     }))
     .pipe(dest(path.resolve(OUTPUT_DIR, 'assets', 'js')))
-    .on('end', () => cb())
+    .on('end', () => cb());
 }
 
-function compile_sass() {
+function compileSass() {
   return src('src//styles/*.scss')
     .pipe(sass())
     .pipe(rename({
       dirname: '',
-      extname: '.css'
+      extname: '.css',
     }))
-    .pipe(dest(path.resolve(OUTPUT_DIR, 'assets', 'css')))
+    .pipe(dest(path.resolve(OUTPUT_DIR, 'assets', 'css')));
 }
 
-function compile_assets(cb) {
+function compileAssets(cb) {
   src('src/assets/img/**')
     .pipe(dest(path.resolve(OUTPUT_DIR, 'assets', 'img')))
-    .on('end', () => cb())
+    .on('end', () => cb());
 }
 
-function watch_files(cb) {
-  watch('src/views/**/*.pug', compile_pug);
-  watch('src/scripts/**/*.ts', compile_ts);
-  watch('src/scripts/**/*.sass', compile_sass);
-  watch('src/assets/**', compile_assets);
-  cb()
+function watchFiles(cb) {
+  watch('src/views/**/*.pug', compilePug);
+  watch('src/scripts/**/*.ts', compileTypescript);
+  watch('src/scripts/**/*.sass', compileSass);
+  watch('src/assets/**', compileAssets);
+  cb();
 }
 
 function startServer(cb) {
-  const handler = require('serve-handler')
-  const http = require('http')
+  const server = http.createServer((req, res) => serveHandler(req, res, { cleanUrls: true, public: 'dist' }));
+  server.listen(3000);
 
-  const server = http.createServer((req, res) => handler(req, res, {cleanUrls: true, public: 'dist'}))
-  server.listen(3000)
-
-  return cb()
-
+  return cb();
 }
 
 function clean(cb) {
   del(OUTPUT_DIR)
-    .then(() => cb())
+    .then(() => cb());
 }
 
 function docs(cb) {
   src(['README.md', './src/scripts/**.ts'])
     .pipe(typedoc({
-      target: "es5",
+      target: 'es5',
       includeDeclarations: true,
-      out: "./docs",
-      name: "rowanieee.org",
+      out: './docs',
+      name: 'rowanieee.org',
       ignoreCompilerErrors: false,
       version: true,
     }))
-    .on('end', () => cb())
+    .on('end', () => cb());
 }
 
-function lint_ts(cb) {
+function lintTypescript(cb) {
   src('src/scripts/**.ts')
-    .pipe(eslint({fix: true}))
+    .pipe(eslint({ fix: true }))
     .pipe(eslint.format())
     .pipe(dest('src/scripts'))
-    .on('error', err => { console.log(err.message); pipe.emit('end')})
-    .on('end', () => cb())
+    .on('error', (err) => { console.log(err.message); })
+    .on('end', () => cb());
 }
 
-function lint_pug() {
+function lintPug() {
   return src('src/views/**/*.pug')
-    .pipe(pugLinter({reporter: 'default', failAfterError: true}))
-    .on('error', err => { console.log(err.message); pipe.emit('end')})
+    .pipe(pugLinter({ reporter: 'default', failAfterError: true }))
+    .on('error', (err) => { console.log(err.message); });
 }
 
-exports.default = series(clean, parallel(lint_ts, lint_pug), parallel(compile_pug, compile_ts, compile_sass, compile_assets))
-exports.start_server = startServer
-exports.watch = series(clean, parallel(compile_pug, compile_ts, compile_sass, compile_assets), startServer, watch_files)
-exports.clean = clean
-exports.docs = docs
-exports.lint_ts = lint_ts
-exports.lint_pug = lint_pug
+exports.default = series(clean,
+  parallel(lintTypescript, lintPug),
+  parallel(compilePug, compileTypescript, compileSass, compileAssets));
+
+exports.start_server = startServer;
+exports.watch = series(clean,
+  parallel(compilePug, compileTypescript, compileSass, compileAssets), startServer, watchFiles);
+
+exports.clean = clean;
+exports.docs = docs;
+exports.lint_ts = lintTypescript;
+exports.lint_pug = lintPug;
